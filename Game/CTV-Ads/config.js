baa27f29f4303a8f0ebf11e458db72dfb27434cd
NisarInvicttus <nisar.saeed@invicttus.com>
2026-03-13 14:24:14 +0500

    revert: App bundle id

--- Game/CTV-Ads/config.js
+++ Game/CTV-Ads/config.js
@@ -23,7 +23,7 @@
         // Static test parameters from sample URL (for development / QA).
         defaultUserAgent: 'Mozilla/5.0 (Linux) AppleWebKit/537.36 (KHTML, like Gecko) Safari/537.36',
         defaultAppName: 'Flappy Wings',
-        defaultAppBundle: 'B0GPCTNP17',
+        defaultAppBundle: 'com.invicttusx.flappywings_v1_ag_game_vega',
         defaultDeviceCategory: 'tv',
         defaultDeviceId: '00000000-0000-0000-0000-000000000000',
 
